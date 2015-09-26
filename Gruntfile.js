6dff24471c6f5d8a91a85ce9857ca9808e981115
Amelia <frevert.amelia@gmail.com>
2015-09-26 17:32:18 -0500

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,10 @@ module.exports = function(grunt) {
                 cwd: "node_modules/",
                 src: [
                     "jquery/dist/jquery.min.js",
-                    "jquery/dist/jquery.min.map"
+                    "jquery/dist/jquery.min.map",
+                    "bootstrap/dist/css/bootstrap.css",
+                    "bootstrap/dist/css/boostrap",
+                    "bootstrap/dist/js/bootstrap.min.js"
                 ],
                 "dest": "server/public/vendor/"
             }
